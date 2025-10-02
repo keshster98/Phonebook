47ef672f513845371a143f3e5b27185850384313
Keshen Nareshchandra <njkeshen98@gmail.com>
2025-10-02 09:58:24 +0800

    Design home fragment XML (search bar, search view, FAB, recycler view for contacts) and toggle only light mode theme

---
+++ app/src/main/java/com/keshen/myapplication/ui/home/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.keshen.myapplication.ui.home
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.keshen.myapplication.databinding.FragmentHomeBinding
+
+class HomeFragment: Fragment() {
+
+    private val viewModel: HomeViewModel by viewModels()
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
