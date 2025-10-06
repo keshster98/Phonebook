1140f7f20c23220ff45b2eaca9dbe953289901be
Keshen Nareshchandra <njkeshen98@gmail.com>
2025-10-06 15:11:17 +0800

    Create contact details fragment, contact details XML as well as relevant icons

---
+++ app/src/main/java/com/keshen/myapplication/ui/details/ContactDetailsFragment.kt
@@ -0,0 +1,19 @@
+package com.keshen.myapplication.ui.details
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.keshen.myapplication.R
+
+class ContactDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contact_details, container, false)
+    }
+}
